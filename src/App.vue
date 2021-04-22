d38916426a51f5a28264d696eda12b4a80a5c58a
Dmitry Sadovsky <dimailn9@gmail.com>
2021-04-22 19:05:04 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
+    <p class="header">Select an adapter</p>
+    <rf-form :resource="resource" class="form">
+      <rf-select-adapter name="mode" :options="engines" />
+    </rf-form>
+
     <v-app>
-      <rf-form :resource="resource" style="display: flex; flex-direction: column;">
+      <rf-form :resource="resource" class="form">
         <rf-select name="mode" :options="engines" />
         <rf-input name="title" />
         <rf-checkbox name="checked" />
+        <rf-datepicker name="date" />
+        <rf-textarea name="description" />
+        <rf-submit />
       </rf-form>
     </v-app>
   </div>
@@ -13,18 +21,31 @@
 <script>
 
 import vuetifyStylesFile from '!file-loader!vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import wrapComponents from './utils/wrap-components'
+import RfSelectAdapter from './components/rf-select-adapter'
+
+
+const styles = {
+  vuetify: vuetifyStylesFile
+}
 
 export default {
   name: 'App',
-  components: wrapComponents(
-    [
-      'input',
-      'select',
-      'checkbox'
-    ]
-  ),
+  components: {
+    ...wrapComponents(
+      [
+        'input',
+        'select',
+        'checkbox',
+        'textarea',
+        'submit',
+        'datepicker'
+      ]
+    ),
+    RfSelectAdapter
+  },
   data(){
     return {
       resource: {
@@ -36,15 +57,17 @@ export default {
   },
   watch: {
     'resource.mode': function() {
+      document.querySelectorAll('#adapter-style').forEach((el) => el.remove())
+
       if(this.resource.mode == 'vuetify') {
+        const style = styles.vuetify
+
         var head = document.getElementsByTagName('head')[0]
         var link = document.createElement('link')
         link.rel = 'stylesheet'
-        link.href = vuetifyStylesFile
-        link.id ='vuetify-style'
+        link.href = style
+        link.id = 'adapter-style'
         head.appendChild(link)
-      } else {
-        document.querySelector('#vuetify-style')?.remove()
       }
     }
   },
@@ -53,11 +76,13 @@ export default {
       return [
         {
           id: 'vrf',
-          title: 'Vrf core'
+          title: 'Vrf core',
+          image: 'logo.png'
         },
         {
           id: 'vuetify',
-          title: 'vrf-vuetify'
+          title: 'vrf-vuetify',
+          image: 'vuetify.svg'
         }
       ]
     }
@@ -66,6 +91,10 @@ export default {
 </script>
 
 <style>
+.header {
+  font-size: 30px;
+  margin-bottom: 25px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -74,4 +103,23 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.form {
+  display: flex;
+  flex-direction: column;
+  /*width: 50%;*/
+  margin: 0 auto;
+}
+
+@media screen and (max-width: 900px) {
+  .form {
+    width: 90%;
+  }
+}
+
+@media screen and (min-width: 900px) {
+  .form {
+    width: 40%;
+  }
+}
+
 </style>
---
+++ src/components/rf-select-adapter.vue
@@ -0,0 +1,51 @@
+<template>
+
+<div style="display: flex">
+  <div
+    class="rf-select-adapter__image"
+    :class="{'rf-select-adapter__image_selected': option.id == $value}"
+    v-for="option in $_options"
+    :key="option.id"
+    @click="onClick(option.id)"
+    style="width: 200px"
+  >
+    <img
+      :src="'/' + option.image"
+      height="100"
+
+    />
+  </div>
+
+
+</div>
+
+</template>
+
+<script>
+
+import {descriptors} from 'vrf'
+
+export default {
+  extends: descriptors.select,
+  methods: {
+    onClick(id) {
+      this.$value = id
+
+      this.$emit('input', id)
+    }
+  }
+}
+
+</script>
+
+<style>
+.rf-select-adapter__image:hover {
+  cursor: pointer;
+}
+
+.rf-select-adapter__image_selected {
+  border-color: #BBDEFB;
+  border-style: solid;
+  border-width: 1px;
+}
+</style>
